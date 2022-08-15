bb92aa4bea98ac94d5987789c83e6febb4bd0237
Ohad Raviv <15645757+uzadude@users.noreply.github.com>
2022-08-15 15:55:51 +0300

    Bugfix to support non string partitions (#62)
    * Bugfix to support non string partitions
    
    * fixing pypandoc version for tests

--- dione-spark/src/test/scala/com/paypal/dione/spark/index/avro/TestAvroIndexManagerBase.scala
+++ dione-spark/src/test/scala/com/paypal/dione/spark/index/avro/TestAvroIndexManagerBase.scala
@@ -10,9 +10,11 @@ class TestAvroIndexManagerBase extends TestIndexManagerBase {
 
   lazy val indexSpec: IndexSpec = IndexSpec("avro_data_tbl", "avro_data_tbl_idx", Seq("id_col"))
 
-  def initDataTable(fieldsSchema: String, partitionFieldSchema: String): Unit = {
-    spark.sql(s"create table ${indexSpec.dataTableName} ($fieldsSchema) partitioned by ($partitionFieldSchema) stored as avro")
+  def initDataTable(fieldsSchema: String, partitionFieldName: String): Unit = {
+    spark.sql(s"create table ${indexSpec.dataTableName} ($fieldsSchema) partitioned by ($partitionFieldName long) stored as avro")
   }
 
   val testSamples = Seq(SampleTest("msg_100", Nil, "var_a_100", 349, 22, 31))
+
+  override val samplePartition: String = "20210203"
 }
